ae851f50d6cad124442f1785f44f00574b0e91ae
iddo <61661956+iddo7@users.noreply.github.com>
2023-08-31 16:06:50 -0400

    Implementing basic static Card Template

--- index.php
+++ index.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    aa
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://hdmusic.cc/uploads/posts/2022-12/1670058726_cover.jpg" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">hyponchodriac</h4>
+                        <p class="card-text">brakence</p>
+                        <p class="card-text">2022-12-02 - 11 chansons</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
