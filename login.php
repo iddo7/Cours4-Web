a6492aee5f449221da4457f9f298c547887de150
iddo7 <isaacnegreiros10@gmail.com>
2023-09-11 13:17:21 -0400

    Implementing Basic verification

--- login.php
+++ login.php
@@ -8,22 +8,80 @@
     <link rel="stylesheet" href="style/style.css">
     <title>Login</title>
 </head>
+<?php 
+    $valuesInputed = array(
+        "username" => "",
+        "password" => "",
+    );
+    $errorOccured = false;
+    $alertMessage = '';
+
+
+    // FORM WAS SUBMITTED
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $inputs = array("username", "password");
+
+        if (anyIsEmpty($inputs)) {
+            $errorOccured = true;
+            $alertMessage = 'Tous les champs doivent être remplis';
+        }
+
+        for ($i = 0; $i < sizeof($inputs); $i++) {
+            $keys = array_keys($valuesInputed);
+            $valuesInputed[$keys[$i]] = trojan($_POST[$inputs[$i]]);
+        }
+
+        if (!$errorOccured) {
+
+        }
+    }
+
+    
+    function anyIsEmpty($arrayOfInputs) {
+        $result = false;
+        foreach ($arrayOfInputs as $input) {
+            if (empty($_POST[$input])) {
+                $result = true;
+                break;
+            }
+        }
+
+        return $result;
+    }
+
+    function trojan($data){
+        $data = trim($data); //Enleve les caractères invisibles
+        $data = addslashes($data); //Mets des backslashs devant les ' et les  "
+        $data = htmlspecialchars($data); // Remplace les caractères spéciaux par leurs symboles comme ­< devient &lt;
+        
+        return $data;
+    }
+?>
 <body>
-                <div class="card p-4 screen-center" style="width: 18rem;">
-                    <h4 class="card-title">Cours4</h4>
-                    <hr>
+    <div class="card p-4 screen-center" style="width: 18rem;">
+        <h4 class="card-title">Cours4</h4>
+        <hr>
 
-                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post" class="">
+        <?php 
+            if ($_SERVER['REQUEST_METHOD'] != 'POST' || $errorOccured == true) {
+        ?>
+                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post" class="">
 
-                        <input type="text" class="form-control mb-3" name="username" id="username" placeholder="Nom d'usager" 
-                            value="">
+                    <input type="text" class="form-control mb-3" name="username" id="username" placeholder="Nom d'usager" 
+                        value="">
 
-                        <input type="password" class="form-control mb-3" name="password" id="password" placeholder="Mot de passe" 
-                            value="">
+                    <input type="password" class="form-control mb-3" name="password" id="password" placeholder="Mot de passe" 
+                        value="">
+                        
+                    <p class="text-<?php echo $errorOccured == true ? "danger" : "success" ?>">
+                        <?php echo $alertMessage; ?>
+                    </p>
 
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+        <?php } ?>
+    </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
